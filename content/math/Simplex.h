687add896883e2e2fa861a0204d8d6a3a6cee8a9
nudetiger <okiathanghoa2006@gmail.com>
2025-10-12 00:39:03 +0700

    dsdsd
    dsds

--- content/math/Simplex.h
+++ content/math/Simplex.h
@@ -1,13 +1,13 @@
 /**
 * Two-phase simplex algorithm for solving linear programs of the form
-*     maximize     cx
-*     subject to   A[j][i].x[i] <= b[j] (x >= 0)
-* INPUT: A -- an m x n matrix
+*      maximize     cx
+*      subject to   A[j][i].x[i] <= b[j] (x >= 0)
+*  in:   A -- an m x n matrix
 *        b -- an m-dimensional vector
 *        c -- an n-dimensional vector
 *        x -- a vector where the optimal solution will be stored
-* OUTPUT: value of the optimal solution (infinity if unbounded above, nan if infeasible)
-* To use this code, create an LPSolver object with A, b, and c as arguments.  Then, call Solve(x).
+*  out: value of the optimal solution (infinity if unbounded above, nan if infeasible)
+*  To use this code, create an LPSolver object with A, b, and c as arguments.  Then, call Solve(x).
 */
 #pragma once 
 
